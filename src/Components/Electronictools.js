04af4501c82be0c4dbe6e17428793221017f257c
Venkata Pradeep Devarampati <165290628+VenkataPradeep43@users.noreply.github.com>
2024-08-01 22:03:24 +0530

    Delete src directory

--- src/Components/Electronictools.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Electronitcools() {
-  return (
-    <>
-    <h1>Electronic tools</h1>
-    </>
-  )
-}
--- src/Components/Farmingtools.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Farmingtools() {
-  return (
-    <>
-        <h1>Farming tools</h1>
-    </>
-  )
-}
